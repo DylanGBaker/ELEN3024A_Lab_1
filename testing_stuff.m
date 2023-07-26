20e26e6fc48ddcc1305d6c4b9a9727db50321758
DylanGBaker <skaduvee32@gmail.com>
2023-07-26 21:05:15 +0200

    frequency spectra now being plotted

--- testing_stuff.m
+++ testing_stuff.m
@@ -1,21 +1,18 @@
 function x = testing_stuff()
-fs = 100;
-t = 0:1/fs:2;
-%m =  4*cos(100*pi*t) + 3*sin(200*pi*t) + 3*sin(300*pi*t);\
-m = cos(100*pi*t);
+fs = 10000;
+t = 0:1/fs:0.2;
+m =  4*cos(100*pi*t) + 3*sin(200*pi*t) + 3*sin(300*pi*t);
 subplot(3,1,1);
 plot(t,m);
-%u = cos(1000*pi*t);
-%subplot(3,1,2);
-%plot(t,u);
-
-M = fft(m);
-n = length(m) - 1;
-for i = 1:length(m)
-    frequency_value(i) = (i/n) * fs;
-    magnitude_value(i) = abs(M(i));
-end
-%x = m.*u;
+u = cos(2000*pi*t);
 subplot(3,1,2);
-plot(frequency_value - ((1/n) * fs),magnitude_value);
+plot(t,u);
+
+AM_signal = m.*u;
+M = fftshift(fft(AM_signal)); 
+n = length(AM_signal) - 1;
+df = fs/n;
+f = -fs/2:df:fs/2;
+subplot(3,1,2);
+plot(f,abs(M)/n);
 end
