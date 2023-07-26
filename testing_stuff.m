8ea4feffb134fe8dad385fb1b5803e389da4c0d8
DylanGBaker <skaduvee32@gmail.com>
2023-07-26 23:57:04 +0200

    Update testing_stuff.m

--- testing_stuff.m
+++ testing_stuff.m
@@ -1,10 +1,10 @@
 function x = testing_stuff()
-fs = 10000;
+fs = 4000000;
 t = 0:1/fs:0.2;
 m =  4*cos(100*pi*t) + 3*sin(200*pi*t) + 3*sin(300*pi*t);
 subplot(3,1,1);
 plot(t,m);
-u = cos(2000*pi*t);
+u = cos(2000000*pi*t);
 subplot(3,1,2);
 plot(t,u);
 
@@ -15,4 +15,5 @@ df = fs/n;
 f = -fs/2:df:fs/2;
 subplot(3,1,2);
 plot(f,abs(M)/n);
+set(gca,'XLim',[999000 1001000]);
 end
