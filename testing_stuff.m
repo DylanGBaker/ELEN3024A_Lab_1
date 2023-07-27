4114dec5698c8ab179523f2ca9cfb7a362304ca0
DylanGBaker <skaduvee32@gmail.com>
2023-07-27 18:14:28 +0200

    Updated DSB-FC function

--- testing_stuff.m
+++ testing_stuff.m
@@ -1,20 +1,24 @@
 function x = testing_stuff()
-fs = 4000000;
+fs = 10000000;
 t = 0:1/fs:0.1;
 m =  4*cos(100*pi*t) + 3*sin(200*pi*t) + 3*sin(300*pi*t);
-subplot(3,1,1);
+subplot(4,1,1);
 plot(t,m);
 u = cos(2000000*pi*t);
-subplot(3,1,2);
+subplot(4,1,2);
 plot(t,u);
-set(gca,'XLim',[0.000001 0.0001]);
+set(gca,'XLim',[0 0.000002]);
 
 AM_signal = m.*u;
+subplot(4,1,3);
+plot(t,AM_signal);
+set(gca,'XLim',[0 0.0001]);
+
 M = fftshift(fft(AM_signal)); 
 n = length(AM_signal) - 1;
 df = fs/n;
 f = -fs/2:df:fs/2;
-subplot(3,1,3);
+subplot(4,1,4);
 plot(f,abs(M)/n);
 set(gca,'XLim',[999000 1001000]);
 end
