3271c73cff48e47312e8ffb5d19ebb0b91eb0d2a
Dylan Baker <skaduvee32@gmail.com>
2023-08-08 13:30:24 +0200

    Fixed it maybe idk

--- testing_stuff_for_two.m
+++ testing_stuff_for_two.m
@@ -1,12 +1,12 @@
 function x = testing_stuff_for_two()
-fs = 4000000;
+fs = 400;
 index = 1;
-lengthOfTime = -3:1/fs:5;
+lengthOfTime = 0:1/fs:10;
 
 t = zeros(size(lengthOfTime));
 f = zeros(size(lengthOfTime));
 xt = zeros(size(lengthOfTime));
-for i = -3:1/fs:5
+for i = 0:1/fs:10
     t(index) = i;
     if i >= 0.5
         xt(index) = exp(-(i-0.5));
@@ -15,30 +15,38 @@ for i = -3:1/fs:5
     end
     index = index + 1;
 end
-Xf = fft(xt);
+Xf = fftshift(fft(xt));
+
 index = 1;
 
 Hf = zeros(size(lengthOfTime));
-for i = -3:1/fs:5
+for i = -5:1/fs:5
     f(index) = i;
-    if i >= -1.5 && i <= 1.5
+    if i <= 1.5 && i >= -1.5
         Hf(index) = cos((pi * i)/3); 
     else
         Hf(index) = 0;
     end
     index = index + 1;
 end
+subplot(4,1,1)
+plot(f,abs(Xf)/(length(Xf) - 1));
 
+subplot(4,1,2)
+plot(f,Hf)
 Mf = Xf .* Hf;
 
-mt = ifftshift(ifft(Mf));
-ct_t = -3:1/fs:5;
-ct = cos(2 * pi * 1000000 * ct_t);
-ut = mt .* ct;
+subplot(4,1,3)
+plot(f,abs(Mf)/(length(Mf) - 1));
 
-n = length(ut) - 1;
-df = fs/n;
-f = -fs/2:df:fs/2;
-plot(f, abs(ut)/n);
-set(gca,'XLim',[-10 10]);
+mt = ifft(ifftshift(Mf));
+ct = cos(2 * pi * 10 * t);
+ut = mt .* ct;
+subplot(4,1,4)
+plot(t, ut)
+%n = length(ut) - 1;
+%df = fs/n;
+%f = -fs/2:df:fs/2;
+%plot(f, abs(ut)/n);
+%set(gca,'XLim',[-10 10]);
 end
